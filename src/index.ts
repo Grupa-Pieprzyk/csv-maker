83d7a03f43e0d969a4dccab70db909d6afacda08
niedzwiedzw <niedzwiedzwo@gmail.com>
2022-07-12 19:43:30 +0200

    cleanup dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-import { } from 'papaparse';
-
-
 export type JSONPrimitive = string | number | boolean | null
 export type JSONValue = JSONPrimitive | JSONObject | JSONArray
 export type JSONObject = { [member: string]: JSONValue }
