e6d5dd7cf158dd060d69dfc66a816ee50aacfa02
niedzwiedzw <niedzwiedzwo@gmail.com>
2022-07-12 16:19:52 +0200

    it doesnt work

---
+++ src/index.ts
@@ -0,0 +1,74 @@
+import { } from 'papaparse';
+
+
+export type JSONPrimitive = string | number | boolean | null
+export type JSONValue = JSONPrimitive | JSONObject | JSONArray
+export type JSONObject = { [member: string]: JSONValue }
+export interface JSONArray extends Array<JSONValue> { }
+
+export type Row = JSONObject;
+
+export type RenderFn<T extends Row> = (v: T) => JSONPrimitive;
+
+export type CosmeticColumn<T extends Row> = {
+  text: string;
+  child: Array<Column<T>>;
+}
+type RendererColumn<T extends Row> = {
+  text: string;
+  child: RenderFn<T>;
+}
+
+type Column<T extends Row> = RendererColumn<T> | CosmeticColumn<T>;
+
+export type Config<T extends Row> = {
+  columns: Array<Column<T>>;
+};
+
+type FlattenedRow = Array<JSONValue>;
+
+function isCosmeticColumn<T extends Row>(column: Column<T>): column is CosmeticColumn<T> {
+  const isCosmetic = (column as CosmeticColumn<T>).child.length == null;
+  return isCosmetic;
+}
+
+function isRenderColumn<T extends Row>(column: Column<T>): column is RendererColumn<T> {
+  const isItReally = !isCosmeticColumn(column);
+  console.debug({ column, isItReally });
+  return isItReally;
+}
+
+export function getPaths<T extends Row>(column: Column<T>): Array<string[]> {
+  if (isRenderColumn(column)) return [[column.text]];
+  return column.child.map((child) => getPaths(child).flatMap((end) => [column.text, ...end]));
+}
+
+function range(length: number): number[] {
+  const range = [];
+  for (let i = 0; i < length; i++) {
+    range.push(i);
+  }
+  return range;
+}
+
+export function flattenRenderer<T extends Row>(column: Column<T>): Array<RendererColumn<T>> {
+  if (isRenderColumn(column)) return [column];
+  return column.child.flatMap((child) => flattenRenderer(child));
+}
+
+
+function getHeaders<T extends Row>(config: Config<T>): Array<FlattenedRow> {
+  const paths = config.columns.flatMap(getPaths);
+  const longest = Math.max(...[...paths.map((v) => v.length)]);
+  return paths.map((path) => path.reverse()).map((path) => [...path, ...range(longest - path.length).map(() => null)]).map((path) => path.reverse())
+}
+
+export function toCSV<T extends Row>(data: T[], config: Config<T>): Array<FlattenedRow> {
+  const headers = getHeaders(config);
+  const values = data.map((v) => config.columns.flatMap(flattenRenderer).flatMap(({ child }) => child(v)));
+  return [
+    ...headers,
+    ...values,
+  ]
+}
+
---
+++ test/blah.test.ts
@@ -0,0 +1,59 @@
+import { toCSV } from '../src';
+
+type Elderliness = 'puppy' | 'adult' | 'elder' | 'dead';
+type Happyness = 'happy' | 'ok' | 'sad';
+type Dog = {
+  name: string;
+  years: {
+    elderliness: Record<string, Elderliness>;
+    happyness: Record<string, Happyness>;
+  }
+}
+
+const data: Dog[] = [
+  {
+    name: 'Doggo',
+    years: { elderliness: { '2021': 'puppy' }, happyness: { '2021': 'happy' } }
+  }
+]
+
+function uniq<T>(collection: T[]): T[] {
+  const alreadyThere: T[] = [];
+  for (let element of collection) {
+    if (alreadyThere.includes(element)) continue;
+    alreadyThere.push(element);
+  }
+  return alreadyThere;
+}
+
+describe('csv rendering', () => {
+  it('works for simple stuff', () => {
+    expect(toCSV(data, {
+      columns: [
+        { text: 'name', child: (dog) => dog.name }
+      ]
+    })).toStrictEqual([
+      ['name'],
+      ['Doggo']
+    ])
+  });
+
+  it('works for more complex stuff', () => {
+    expect(toCSV(data, {
+      columns: [
+        { text: 'name', child: (dog) => dog.name },
+        ...uniq(data.flatMap((dog) => Object.keys(dog.years))).map((year) => ({
+          text: year, child: [
+            { text: 'happiness', child: (dog: Dog) => dog.years.happyness[year] ?? null },
+            { text: 'elderliness', child: (dog: Dog) => dog.years.elderliness[year] ?? null },
+          ]
+        }))
+      ]
+    })).toStrictEqual([
+      [null, '2021', null],
+      ['name', 'happiness', 'elderliness'],
+      ['Doggo', 'happy', 'puppy']
+
+    ])
+  })
+});
